630ee581c59386fae5482181aa370833a53dd548
= <=>
2021-02-21 07:21:29 +0700

    add 3 Controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        echo "Nama : Mochammad Fajar Darussalam";
+        echo "<br>";
+        echo "NIM  : 1941720004";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WelcomeController;
+// use App\Http\Controllers\WelcomeController;
+// use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,17 +22,21 @@ use App\Http\Controllers\WelcomeController;
 //     return view('welcome');
 // });
 
-Route::get('/', function (){
-    echo "Selamat Datang";
-});
+// Route::get('/', function (){
+//     echo "Selamat Datang";
+// });
 
-Route::get('/about', function (){
-    echo "NIM  : 1941720004";
-    echo "<br>";
-    echo "Nama : Mochammad Fajar Darussalam";
-});
+// Route::get('/about', function (){
+//     echo "NIM  : 1941720004";
+//     echo "<br>";
+//     echo "Nama : Mochammad Fajar Darussalam";
+// });
 
-Route::get('/articles/{id}', function ($id){
-    echo "Halaman Artikel dengan ID ".$id;
-});
+// Route::get('/articles/{id}', function ($id){
+//     echo "Halaman Artikel dengan ID ".$id;
+// });
+
+Route::get('/',[HomeController::class, 'index']);
+Route::get('/about',[AboutController::class, 'about']);
+Route::get('/articles/{id}',[ArticleController::class, 'articles']);
 
